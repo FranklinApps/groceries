2a4bba5f72927a99579dabcc32031a35c1171cbd
Frank <Franklinstevennealjr@gmail.com>
2022-07-15 22:45:22 -0600

    backend update

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -18,5 +18,85 @@ module.exports = {
             res.status(400).json(err)
         })
     },
+    login: (req, res)=> {
+        User.fineOne({email: req.body.email})
+        .then((userRecord)=>{
+            if(userRecord === null){
+                res.status(400).json({message: "Invalid login"})
+            } else{
+                bcrypt.compare(req.body.password, userRecord.password)
+                .then((isPasswordValid)=>{
+                    if(isPasswordValid){
+                        console.log("Password passed");
+                        res.cookie(
+                            "userToken",
+                            jwt.sign(
+                                {
+                                    id: userRecord._id,
+                                    email: userRecord.email
+                                },
+                                process.env.JWT_SECRET
+                            ),{
+                                httpOnly:true,
+                                expires: newDate(Date.now() + 9000000)
+                            }
+                        ).json({
+                            message: "success",
+                            userLoggedIn: userRecord.email
+                        });
+                    } else{
+                        res.status(400).json({message: "Invalid Attempt"})
+                    }
+                })
+                .catch((err)=>{
+                    console.log(err);
+                    res.status(400).json({message: "Invalid Attempt"})
+                })
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.status(400).json({message: "Invalid Attempt"})
+        })
+    },
 
+    logout: (req,res) => {
+        console.log("logging out");
+        res.clearCookie("userToken");
+        res.json({
+            message: "You are now logged out"
+        });
+    },
+    getLoggedInUser: (req, res)=>{
+        User.findOne({_id: req.jwtpayload.id})
+            .then((user)=>{
+                console.log(user);
+                res.json(user)
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+
+    },
+
+    findAllUsers: (req, res) => {
+        User.find()
+            .then((allUsers) => {
+                res.json(allUsers);
+            })
+            .catch((err) => {
+                console.log("Find All Users failed");
+                res.json({ message: "Something went wrong in findAll", error: err })
+            })
+    },
+
+    groceryItems: (req,res) => {
+        User.findByIdAndUpdate({_id: req.jwtpayload.id},req.body,{new:true})
+            .then((groceries)=>{
+                res.json(groceries)
+            })
+            .catch((err)=>{
+                res.json({message: "Failed to enter into grocery items", err})
+            })
+    }
 }
